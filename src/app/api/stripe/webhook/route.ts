0dbd97bf60fb5c4172275417d9fb5b8e436a0d23
ManuelSantoroG <149789965+ManuelSantoroG@users.noreply.github.com>
2025-08-17 15:47:03 +0100

    removi  logs para testar webhook 2

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -15,40 +15,24 @@ export const POST = async (request: Request) => {
   }
   const text = await request.text();
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-  let event: Stripe.Event;
-
-  try {
-    event = stripe.webhooks.constructEvent(
-      text,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET,
-    );
-  } catch (err) {
-    console.error("❌ Erro ao construir evento do Stripe:", err);
-    return NextResponse.error();
-  }
-
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET,
+  );
   if (event.type === "checkout.session.completed") {
     console.log("Checkout session completed");
     const session = event.data.object as Stripe.Checkout.Session;
     const orderId = session.metadata?.orderId;
     if (!orderId) {
-      console.error("❌ orderId não encontrado na session.metadata");
       return NextResponse.error();
     }
-
-    console.log("Conectando ao banco para atualizar pedido:", orderId);
-
-    try {
-      await db
-        .update(orderTable)
-        .set({ status: "paid" })
-        .where(eq(orderTable.id, orderId));
-      console.log("✅ Pedido atualizado:", orderId);
-    } catch (err) {
-      console.error("❌ Erro ao atualizar pedido no banco:", err);
-    }
+    await db
+      .update(orderTable)
+      .set({
+        status: "paid",
+      })
+      .where(eq(orderTable.id, orderId));
   }
-
   return NextResponse.json({ received: true });
 };
